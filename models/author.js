aed94af242f65136cd492cd4fa998fc1f1b32db5
nodonut <apoorvsohal@protonmail.com>
2021-07-21 12:02:32 +0530

    initial commit

---
+++ models/author.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+AuthorSchema.virtual('lifespan').get(function () {
+  let lifetime_string = '';
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+  }
+  lifetime_string += ' - ';
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+  }
+  return lifetime_string;
+});
+
+AuthorSchema.virtual('url').get(function () {
+  return `/catalog/author/${this._id}`;
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
