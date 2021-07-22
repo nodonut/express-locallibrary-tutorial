c2c1a6de363577b197728a971dc6ca81f885e6a3
nodonut <apoorvsohal@protonmail.com>
2021-07-22 11:37:47 +0530

    created controllers and routes

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
 const MONGODB_PASS = process.env.MONGODB_PASS;
 const mongoDB = `mongodb+srv://apoorv:${MONGODB_PASS}@cluster0.h1lql.mongodb.net/local_library?retryWrites=true&w=majority`;
@@ -29,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
