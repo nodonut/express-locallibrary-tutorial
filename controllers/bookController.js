baa9cbaca120656a77002840b22d055232d93563
nodonut <apoorvsohal@protonmail.com>
2021-07-23 19:05:07 +0530

    added author and book detail view

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,8 +1,9 @@
+const async = require('async');
+
 const Book = require('../models/book');
 const Author = require('../models/author');
 const Genre = require('../models/genre');
 const BookInstance = require('../models/bookinstance');
-const async = require('async');
 
 exports.index = function (req, res) {
   async.parallel(
@@ -43,7 +44,31 @@ exports.book_list = function (req, res) {
 
 // Display detail page for a specific book.
 exports.book_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+  async.parallel(
+    {
+      book: function (callback) {
+        Book.findById(req.params.id).populate('author').populate('genre').exec(callback);
+      },
+      book_instance: function (callback) {
+        BookInstance.find({ book: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+
+      if (results.book == null) {
+        const err = new Error('Book not found');
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render('book_detail', {
+        title: results.book.title,
+        book: results.book,
+        book_instances: results.book_instance,
+      });
+    }
+  );
 };
 
 // Display book create form on GET.
