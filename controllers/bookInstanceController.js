80430bbdfb1d95f16aecd6645a0f310b8a5b3709
nodonut <apoorvsohal@protonmail.com>
2021-08-05 18:22:01 +0530

    feature: added update for book instance

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,5 +1,5 @@
 const { body, validationResult } = require('express-validator');
-
+const async = require('async');
 const Book = require('../models/book');
 const BookInstance = require('../models/bookinstance');
 
@@ -43,7 +43,10 @@ exports.bookinstance_create_get = function (req, res, next) {
   Book.find({}, 'title').exec(function (err, books) {
     if (err) return next(err);
 
-    res.render('bookinstance_form', { title: 'Create BookInstance', book_list: books });
+    res.render('bookinstance_form', {
+      title: 'Create BookInstance',
+      book_list: books,
+    });
   });
 };
 
@@ -51,9 +54,15 @@ exports.bookinstance_create_get = function (req, res, next) {
 exports.bookinstance_create_post = [
   // Validate and sanitise fields.
   body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
-  body('imprint', 'Imprint must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
   body('status').escape(),
-  body('due_back', 'Invalid date').optional({ checkFalsy: true }).isISO8601().toDate(),
+  body('due_back', 'Invalid date')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
 
   (req, res, next) => {
     const errors = validationResult(req);
@@ -117,14 +126,81 @@ exports.bookinstance_delete_post = function (req, res, next) {
 
 // Display BookInstance update form on GET.
 exports.bookinstance_update_get = function (req, res, next) {
-  Book.find({}, 'title').exec(function (err, books) {
-    if (err) return next(err);
+  async.parallel(
+    {
+      books: function (callback) {
+        Book.find(callback);
+      },
+      book_instance: function (callback) {
+        BookInstance.findById(req.params.id).populate('book').exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) return next(err);
 
-    res.render('bookinstance_form', { title: 'Create BookInstance', book_list: books });
-  });
+      if (results.books == null) {
+        const err = new Error('Book not found');
+        err.status = 404;
+        return next(err);
+      } else {
+        res.render('bookinstance_form', {
+          title: 'Update Book Instance',
+          book_list: results.books,
+          selected_book: results.book_instance.book._id,
+          bookinstance: results.book_instance,
+        });
+      }
+    }
+  );
 };
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance update POST');
-};
+exports.bookinstance_update_post = [
+  body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+  body('imprint', 'Imprint must be specified')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('status').escape(),
+  body('due_back', 'Invalid date')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      Book.find({}, 'title').exec(function (err, books) {
+        if (err) {
+          return next(err);
+        }
+        res.render('bookinstance_form', {
+          title: 'Create BookInstance',
+          book_list: books,
+          selected_book: bookInstance.book._id,
+          errors: errors.array(),
+          bookinstance: bookInstance,
+        });
+      });
+      return;
+    } else {
+      BookInstance.findByIdAndUpdate(
+        req.params.id,
+        bookInstance,
+        {},
+        function (err, thebookinstance) {
+          if (err) return next(err);
+          res.redirect(thebookinstance.url);
+        }
+      );
+    }
+  },
+];
